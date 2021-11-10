5bf24474b984c55209d2f57290bff1f1e02cad7b
jako5457 <jako5457@gmail.com>
2021-11-10 20:59:17 +0100

    Add project files.

---
+++ XamarinBehaviorsEksempel/XamarinBehaviorsEksempel/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinBehaviorsEksempel.ViewModels;
+
+namespace XamarinBehaviorsEksempel.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ XamarinBehaviorsEksempel/XamarinBehaviorsEksempel/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XamarinBehaviorsEksempel.Models;
+using XamarinBehaviorsEksempel.ViewModels;
+
+namespace XamarinBehaviorsEksempel.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
